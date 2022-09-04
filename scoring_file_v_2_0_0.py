350259bdfd969bd7cf412f51c85c41c9a157d91e
elidrissim <111313008+elidrissim@users.noreply.github.com>
2022-09-04 18:34:22 +0100

    Update scoring_file_v_2_0_0.py

--- scoring_file_v_2_0_0.py
+++ scoring_file_v_2_0_0.py
@@ -14,42 +14,19 @@ st.markdown(f'<h1 style="color:#da9954;text-align: center;font-size:36px;">{"Ind
 st.markdown(f'<h1 style="color:#557caf;font-size:24px;">{"> Realized by: Mustapha El Idrissi"}</h1>', unsafe_allow_html=True)
 
 
-def user_input_features():
-    Pregnancies = st.sidebar.slider("Pregnancies", 0, 25)
-    Glucose = st.sidebar.slider("Glucose", 0, 300)
-    BloodPressure = st.sidebar.slider("Glucose", 0, 150)
-    SkinThickness = st.sidebar.slider("Skin Thickness", 0, 100)
-    Insulin = st.sidebar.slider("Insulin", 0, 1000)
-    BMI = st.sidebar.slider("BMI", 0.0, 100.0, 0.1)
-    DiabetesPedigreeFunction = st.sidebar.slider("Diabetes Pedigree Function", 0.00, 10.00, 0.01)
-    Age = st.sidebar.slider("Age", 0, 120)
+st.sidebar.slider("Pregnancies", 0, 25)
+st.sidebar.slider("Glucose", 0, 300)
+st.sidebar.slider("Glucose", 0, 150)
+st.sidebar.slider("Skin Thickness", 0, 100)
+st.sidebar.slider("Insulin", 0, 1000)
+st.sidebar.slider("BMI", 0.0, 100.0, 0.1)
+st.sidebar.slider("Diabetes Pedigree Function", 0.00, 10.00, 0.01)
+st.sidebar.slider("Age", 0, 120)
 
 
-    data = {"Pregnancies": Pregnancies,
-            "Glucose": Glucose,
-            "Blood Pressure": BloodPressure,
-            "Skin Thickness": SkinThickness,
-            "Insulin": Insulin,
-            "BMI": BMI,
-            "Diabetes Pedigree Function": DiabetesPedigreeFunction,
-            "Age": Age}
-
-    features = pd.DataFrame(data, index=[0])
-    return features
 
 def show_results():
     st.subheader("User Input parameters")
     st.write("Outcome")
-    #model_Outcome = pickle.load(open("model.pkl", "rb"))
-    #prediction = model_Outcome.predict(Outcome)
-    #prediction_proba = model_Outcome.predict_proba(Outcome)
-    #st.subheader("Class labels and their corresponding index number")
-    #st.write(pd.DataFrame(model_Outcome.classes_))
-    #st.subheader("Prediction")
-    #st.write(prediction)
-    #st.subheader("Prediction Probability")
-    #st.write(prediction_proba)
 
-
-#Outcome = user_input_features()
 show_results()
